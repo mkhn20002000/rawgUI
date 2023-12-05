56b6f2a2445ad429ce7c4a2c68dfa86e0dee4af2
Manoochehr Khatami <manouchehr.khatami@gmail.com>
2023-12-05 18:34:58 +0100

    Display Genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,8 +12,8 @@ const GameGrid = () => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing="10px"
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        spacing={3}
         padding="10px"
       >
         {isLoading &&
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,24 @@
-import useGenres from "../hooks/useGenres"
-
+import useGenres from "../hooks/useGenres";
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import getCroppedImageUrl from "../services/image-url";
 const GenreList = () => {
-   const {data} = useGenres()
+  const { data } = useGenres();
   return (
-    <ul>
-        {data.map( genre => <li key={genre.id}>{genre.name}</li>)}
-    </ul>
-  )
-}
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              src={getCroppedImageUrl(genre.image_background)}
+              boxSize="32px"
+              borderRadius={8}
+            ></Image>
+            <Text fontSize='lg'>{genre.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
 
-export default GenreList
+export default GenreList;

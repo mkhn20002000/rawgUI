f66b8443d697ef20aa0a043a43953ffe1eda81ea
Manoochehr Khatami <manouchehr.khatami@gmail.com>
2023-12-05 11:10:05 +0100

    get optimized images

---
+++ src/services/image-url.ts
@@ -0,0 +1,5 @@
+const getCroppedImageUrl = (url: string) => {
+    const index = url.indexOf('media/') + 'media/'.length;
+    return url.slice(0,index) + 'crop/600/400/' + url.slice(index)
+}
+export default getCroppedImageUrl;

698d61f06a012ab78d1ba657bed21bb153d2be46
Manoochehr Khatami <manouchehr.khatami@gmail.com>
2023-12-06 10:21:55 +0100

    Filtering by genre

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import './App.css'
 import NavBar from './components/NavBar'
 import GameGrid from './components/GameGrid'
 import GenreList from './components/GenreList'
+import { useState } from 'react'
+import { Genre } from './hooks/useGenres'
 
 function App() {
 
+  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null)
   return (
     <>
       <Grid templateAreas={{
@@ -23,11 +26,11 @@ function App() {
         </GridItem>
         <Show above='lg'>
           <GridItem area='aside' paddingX={5} >
-            <GenreList />
+            <GenreList onSelctGenre={(genre) => setSelectedGenre(genre)}/>
           </GridItem>
         </Show>
         <GridItem area='main' >
-          <GameGrid />
+          <GameGrid selectedGenre={selectedGenre}/>
         </GridItem>
       </Grid>
     </>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,7 +1,11 @@
-import useGenres from "../hooks/useGenres";
-import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
+import { Button, HStack, Image, List, ListItem, Spinner } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
-const GenreList = () => {
+
+interface Props {
+  onSelctGenre: (genre: Genre) => void;
+}
+const GenreList = ({onSelctGenre}: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -18,7 +22,7 @@ const GenreList = () => {
                 boxSize="32px"
                 borderRadius={8}
               ></Image>
-              <Text fontSize="lg">{genre.name}</Text>
+              <Button onClick={() => onSelctGenre(genre)} variant='link' fontSize="lg">{genre.name}</Button>
             </HStack>
           </ListItem>
         ))}

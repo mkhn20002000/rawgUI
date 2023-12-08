41bdfe454ed9c5cbc92a394eb8fb5f9348c8c321
mkhn-person2 <orkidehartgallery2023@gmail.com>
2023-12-08 17:19:49 +0100

    add emojis

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,22 +3,27 @@ import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
 
 interface Props {
   game: Game;
 }
 const GameCard = ({ game }: Props) => {
   return (
-      <Card>
-        <Image src={getCroppedImageUrl(game.background_image)} />
-        <CardBody>
-          <HStack marginBottom={3} justifyContent='space-between'>
-              <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
-              <CriticScore score={game.metacritic}/>
-          </HStack>
-          <Heading fontSize="2xl">{game.name}</Heading>
-        </CardBody>
-      </Card>
+    <Card>
+      <Image src={getCroppedImageUrl(game.background_image)} />
+      <CardBody>
+        <HStack marginBottom={3} justifyContent="space-between">
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
+        <Heading fontSize="2xl">
+          {game.name} <Emoji rating={game.rating_top} />
+        </Heading>
+      </CardBody>
+    </Card>
   );
 };
 
